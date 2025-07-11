9f0f80c076b59615053521b55da25bc1fd126225
Oumayma Metoui <oumayma.metoui@gmail.com>
2025-07-11 16:16:36 +0100

    new version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,37 +5,40 @@ import Button from '../components/Button';
 import cv from '../assets/pdf/CV_Oumayma_Metoui_Developpeur_FullStack.pdf'
 export default function Home() {
   return (
-    <div className='p-2 container mx-auto'>
-                
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-           
+    <section
+  id="home"
+  className="min-h-screen flex flex-col-reverse md:flex-row items-center justify-center gap-8 px-6 py-10 bg-white dark:bg-gray-900"
+>
+  {/* Texte et bouton */}
+  <div className="text-center md:text-left max-w-xl space-y-6">
+    <h1 className="text-3xl md:text-5xl font-bold text-night-blue dark:text-white">
+      Bonjour, je suis <br />
+      <span className="text-sky-500">Oumayma Metoui</span>
+    </h1>
 
-            <div class=" p-4  ">
-              <h2 className='pb-6 '>
-                <Typewriter
-                  words={['Bonjour', 'Je suis développeur Full Stack']}
-                  loop={true}
-                  cursor
-                  cursorStyle="|"
-                  typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={1000}  />
-              </h2>
-              <Button href={cv} type="button"  >Telecharger le CV</Button>
+    <p className="text-lg md:text-xl text-gray-700 dark:text-gray-300">
+      Développeuse Full Stack JavaScript passionnée par la création
+      d’expériences web modernes.
+    </p>
 
-               
-            </div>
+    <a
+      href={cv}
+      download
+      className="inline-block bg-night-blue text-white px-6 py-3 rounded hover:bg-sky-600 transition"
+    >
+      Télécharger le CV
+    </a>
+  </div>
 
-            <div class=" p-4  mx-auto ">
-                <img src={img} alt=""          
-                className="w-64 h-64 object-cover shadow-lg" />
-            </div>
+  {/* Image */}
+  <div className="w-48 h-48 md:w-64 md:h-64 rounded-full overflow-hidden shadow-lg">
+    <img
+      src={img}
+      alt="Oumayma Metoui portrait"
+      className="w-full h-full object-cover"
+    />
+  </div>
+</section>
 
-           
-
-        </div>
-
-
-    </div>
   )
 }
