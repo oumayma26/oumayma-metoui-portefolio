b85ff30db954b15aec9cb8005a764a06f600c64e
Oumayma Metoui <oumayma.metoui@gmail.com>
2025-07-14 12:02:48 +0100

    footer updated

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,15 @@
+import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
+  const { t } = useTranslation();
+  
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-slate-200 px-4 py-3 shadow z-50">
       <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-center items-center gap-4">
         
         {/* Texte de copyright */}
         <div className="text-night-blue text-sm text-center sm:text-left">
-          &copy; {new Date().getFullYear()} Oumayma. Tous droits réservés.
+          &copy; {new Date().getFullYear()} Oumayma. {t('Tous droits réservés')}
         </div>
       </div>
     </footer>
